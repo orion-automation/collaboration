e13ea178c4f863461502ffb2c1bcfc28b667bc9d
T.C. <tonny_1983@hotmail.com>
2025-12-09 09:38:14 +0800

    add missing files

---
+++ src/main/java/com/eorion/bo/enhancement/collaboration/domain/dto/openapi/IdDTOLong.java
@@ -0,0 +1,6 @@
+package com.eorion.bo.enhancement.collaboration.domain.dto.openapi;
+
+import com.eorion.bo.enhancement.collaboration.domain.dto.outbound.IdDTO;
+
+public class IdDTOLong extends IdDTO<Long> {
+}
